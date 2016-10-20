fbb56aad7c7f8229a25eec0d5d2c3d221bc97045
S. Zhu <stevenzhu@utexas.edu>
2016-10-20 14:17:08 -0500

    Fix minor formatting

--- Driver/Main.c
+++ Driver/Main.c
@@ -3,26 +3,44 @@
 #include <RASLib/inc/time.h>
 #include <RASLib/inc/servo.h>
 
+static tBoolean initialized = false;
 static tServo *leftServo;
 static tServo *rightServo;
-static tBoolean initialized = false;
-static tADC *leftIRSensor;
-static tADC *rightIRSensor;
+static tADC *leftSensor;
+static tADC *rightSensor;
 static wallDistance = 10;
+
 void setup(void) {
     if (initialized) {
         return;
     }
 
     initialized = true;
-    
+
     leftServo = InitializeServo(PIN_B1);
     rightServo  = InitializeServo(PIN_B2);
-    leftIRSensor = InitializeADC(PIN_B3);
-    rightIRSensor = InitializeADC(PIN_B4);
+    leftSensor = InitializeADC(PIN_B3);
+    rightSensor = InitializeADC(PIN_B4);
 }
 
-
+void wallAdjust(void) {
+  if (ADCRead(leftSensor) < ADCRead(rightSensor)) {
+      if (ADCRead(leftSensor) < wallDistance) {
+          leftPosition += 0.01f;
+      }
+      else if (ADCRead(leftSensor) > wallDistance) {
+          leftPosition -= 0.01f;
+      }
+  }
+  else if (ADCRead(leftSensor) > ADCRead(rightSensor)) {
+      if (ADCRead(rightSensor) < wallDistance) {
+          rightPosition -= 0.01f;
+      }
+      else if (ADCREad(leftSensor) < wallDistance) {
+          rightPosition += 0.01f;
+      }
+  }
+}
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
@@ -31,22 +49,7 @@ int main(void) {
     float rightPosition = 1;
 
     while (1) {
-        if (ADCRead(leftIRSensor) < ADCRead(rightIRSensor)) {
-            if (ADCRead(leftSensor) < wallDistance) {
-                leftPosition += 0.01f;
-            }
-            else if (ADCRead(leftSensor) > wallDistance) {
-                leftPosition -= 0.01f;
-            }
-        }
-        else {
-            if (ADCRead(rightSensor) < wallDistance) {
-                rightPosition -= 0.01f;
-            }
-            else if (ADCREad(leftSensor) < wallDistance) {
-                rightPosition += 0.01f;
-            }
-        }
+        wallAdjust();
 
         SetServo(leftServo,leftPosition);
         SetServo(rightServo,rightPosition);
