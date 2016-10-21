bd20e71ce3feb1c4ce44fb81b323ecc40d07deb6
S. Zhu <stevenzhu@utexas.edu>
2016-10-20 20:52:32 -0500

    Fix infinity speed increase/descrease and formatting

--- Driver/Main.c
+++ Driver/Main.c
@@ -17,49 +17,55 @@ float leftPosition;
 float rightPosition;
 
 void setup(void) {
-    if (initialized) {
-        return;
-    }
+  if (initialized) {
+    return;
+  }
 
-    initialized = true;
+  initialized = true;
 
-    wallDistance = 10;
-    leftPosition = 0;
-    rightPosition = 1;
-    leftServo = InitializeServo(PIN_B1);
-    rightServo  = InitializeServo(PIN_B2);
-    leftSensor = InitializeADC(PIN_B3);
-    rightSensor = InitializeADC(PIN_B4);
+  wallDistance = 10;
+  leftPosition = 0;
+  rightPosition = 1;
+  leftServo = InitializeServo(PIN_B1);
+  rightServo  = InitializeServo(PIN_B2);
+  leftSensor = InitializeADC(PIN_B3);
+  rightSensor = InitializeADC(PIN_B4);
 }
 
 void wallAdjust(void) {
   if (ADCRead(leftSensor) < ADCRead(rightSensor)) {
-      if (ADCRead(leftSensor) < wallDistance) {
-          leftPosition += 0.01f;
-      }
-      else if (ADCRead(leftSensor) > wallDistance) {
-          leftPosition -= 0.01f;
-      }
+    if (ADCRead(leftSensor) < wallDistance) {
+      leftPosition += 0.01f;
+    }
+    else if (ADCRead(leftSensor) > wallDistance) {
+      leftPosition -= 0.01f;
+    }
+    else {
+      leftPosition = 0;
+    }
   }
   else if (ADCRead(leftSensor) > ADCRead(rightSensor)) {
-      if (ADCRead(rightSensor) < wallDistance) {
-          rightPosition -= 0.01f;
-      }
-      else if (ADCRead(leftSensor) < wallDistance) {
-          rightPosition += 0.01f;
-      }
+    if (ADCRead(rightSensor) < wallDistance) {
+      rightPosition -= 0.01f;
+    }
+    else if (ADCRead(leftSensor) < wallDistance) {
+      rightPosition += 0.01f;
+    }
+    else {
+      rightPosition = 1;
+    }
   }
 }
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
-    setup();
+  setup();
 
-    while (1) {
-        wallAdjust();
+  while (1) {
+    wallAdjust();
 
-        // Set servo speed
-        SetServo(leftServo,leftPosition);
-        SetServo(rightServo,rightPosition);
-    }
+      // Set servo speed
+    SetServo(leftServo,leftPosition);
+    SetServo(rightServo,rightPosition);
+  }
 }
