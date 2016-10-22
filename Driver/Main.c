ba94c356a719107f49d84832737ea1b56ed9c688
Steven <stevenzhu@utexas.edu>
2016-10-22 17:17:05 -0500

    improve wall following

--- Driver/Main.c
+++ Driver/Main.c
@@ -38,44 +38,66 @@ void setup(void) {
 	rightSensor = InitializeADC(PIN_D1);
 }
 
-void leftTurn(int factor) {
-	leftPosition = factor*0.55;
-	rightPosition = factor*0.05;
+void leftTurn(float factor) {
+	//Printf("Turning left ");
+	leftPosition =  0.95-factor*0.40;
+	rightPosition = 0.45-factor*0.40;
 }
 
-void rightTurn(int factor) {
-	leftPosition = factor*0.95;
-	rightPosition = factor*0.45;
+void rightTurn(float factor) {
+	//Printf("Turning right ");
+	leftPosition = 0.55+factor*0.40;
+	rightPosition = 0.05+factor*0.40;
 }
 
 void wallAdjust(void) {
-    float current = 7.83/ADCRead(leftSensor)-1.66;
-    float factor = abs(current-wallDistance)/wallDistance;
+	float current = 7.83/ADCRead(leftSensor)-1.66;
+	float factor = abs(current-wallDistance)/wallDistance;
+	//Printf("%f ",factor);
+	if (factor > 1) {factor = 1;}
 //    Printf("%f,%f\n",current,ADCRead(leftSensor));
-    if (abs(current-wallDistance)<abs(last-wallDistance)) {
-	if (lastOp = 1) {leftTurn(factor);}
-	else {rightTurn(factor);}
-    }
-    else if (abs(current-wallDistance)>abs(last-wallDistance)) {
-	if (lastOp = 1) {rightTurn(factor);}
-	else {leftTurn(factor);}
+
+	if (current == wallDistance) {
+		leftPosition = 1.0f;
+		rightPosition = 0.0f;
     }
     else {
-        leftPosition = 1.0f;
-        rightPosition = 0.0f;
-    }
-    Printf("%f %f\n",leftPosition,rightPosition);
+		if (abs(current-wallDistance)<abs(last-wallDistance)) {
+			if (lastOp == 1) {
+				leftTurn(factor);
+				lastOp=1;
+			}
+			else {
+				rightTurn(factor);
+				lastOp=2;
+			}
+	    }
+	    if (abs(current-wallDistance)>abs(last-wallDistance)) {
+			if (lastOp == 1) {
+				rightTurn(factor);
+				lastOp=2;
+			}
+			else {
+				leftTurn(factor);
+				lastOp=1;
+			}
+	    }
+	}
+
+    last = current;
 }
 
 int main(void) {
     // Initialization code can go here
-  setup();
+	setup();
 
-  while (1) {
-    wallAdjust();
-    // Set servo speed
-    SetServo(leftServo,leftPosition);
-    SetServo(rightServo,rightPosition);
-    //Wait(0.1);
-  }
+	while (1) {
+		wallAdjust();
+		//Printf("%f,%f\n",leftPosition,rightPosition);
+
+
+		SetServo(leftServo,leftPosition);
+		SetServo(rightServo,rightPosition);
+		Wait(0.1);
+	}
 }
